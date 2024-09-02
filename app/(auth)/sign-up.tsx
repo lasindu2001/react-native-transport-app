a5b602f118beaee3b79e78512e94f0576aa5c7cf
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-03 00:44:10 +0530

    create oauth component

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
 import { Link } from "expo-router";
 import { useState } from "react";
@@ -56,7 +57,7 @@ const SignUp = () => {
                         onPress={onSignUpPress}
                         className="mt-6"
                     />
-                    {/* <OAuth /> */}
+                    <OAuth />
                     <Link
                         href="/sign-in"
                         className="text-lg text-center text-general-200 mt-10"
