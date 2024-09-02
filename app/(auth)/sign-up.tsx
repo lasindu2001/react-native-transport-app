2cf275c5526dcb5b7ca5ef9def7b32e63ecedad1
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-09-02 23:51:26 +0530

    create signup page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,74 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
-  return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
-    </SafeAreaView>
-  );
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const onSignUpPress = () => { }
+
+    return (
+        <ScrollView className="flex-1 bg-white">
+            <View className="flex-1 bg-white">
+                <View className="relative w-full h-[250px]">
+                    <Image
+                        source={images.signUpCar}
+                        className="z-0 w-full h-[250px]"
+                    />
+                    <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+                        Create Your Account
+                    </Text>
+                </View>
+                <View className="p-5">
+                    <InputField
+                        label="Name"
+                        placeholder="Enter name"
+                        icon={icons.person}
+                        value={form.name}
+                        onChangeText={(value) => setForm({ ...form, name: value })}
+                    />
+                    <InputField
+                        label="Email"
+                        placeholder="Enter email"
+                        icon={icons.email}
+                        textContentType="emailAddress"
+                        value={form.email}
+                        onChangeText={(value) => setForm({ ...form, email: value })}
+                    />
+                    <InputField
+                        label="Password"
+                        placeholder="Enter password"
+                        icon={icons.lock}
+                        secureTextEntry={true}
+                        textContentType="password"
+                        value={form.password}
+                        onChangeText={(value) => setForm({ ...form, password: value })}
+                    />
+                    <CustomButton
+                        title="Sign Up"
+                        onPress={onSignUpPress}
+                        className="mt-6"
+                    />
+                    {/* <OAuth /> */}
+                    <Link
+                        href="/sign-in"
+                        className="text-lg text-center text-general-200 mt-10"
+                    >
+                        Already have an account?{" "}
+                        <Text className="text-primary-500">Log In</Text>
+                    </Link>
+                </View>
+                {/* Verification Model */}
+            </View>
+        </ScrollView>
+    );
 };
 
 export default SignUp;
---
+++ components/OAuth.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OAuth = () => {
+  return (
+    <View>
+      <Text>OAuth</Text>
+    </View>
+  )
+}
+
+export default OAuth
